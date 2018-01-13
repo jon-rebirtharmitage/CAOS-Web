025bb3eff76c2ddbbd093bdc39c0e0f8d7c0293a
root <jon@rebirtharmitage.com>
2018-01-13 14:08:31 -0600

    Working version Alpha 0.0.1

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 import(
-
+	"html/template"
+	"net/http"
+	//Uncomment for Verbose logging
+	//"fmt"
 )
 
 /*
@@ -17,42 +20,22 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body: "blank..."}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/" + title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	p, _ := loadPage("Test")
+  renderTemplate(w, "./frontend/index", p)
 }
-
-var templates = template.Must(template.ParseFiles("./html/index.html", "./html/view.html", "./html/editsmall.html","./html/results.html", "./html/login.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-var validPath = regexp.MustCompile("^/(index|editsmall|view|results)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
 }
 
 func main() {
-  http.HandleFunc("/", makeHandler(viewHandler))
-	http.Handle("/css/",http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
-	http.Handle("/fonts/",http.StripPrefix("/fonts/", http.FileServer(http.Dir("./fonts"))))
-	http.Handle("/js/",http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
-	http.Handle("/img/",http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
-	http.ListenAndServeTLS(":8085", "cert.pem", "privkey.pem", nil)
+  http.HandleFunc("/", viewHandler)
+	http.Handle("/css/",http.StripPrefix("/css/", http.FileServer(http.Dir("./frontend/css"))))
+	http.Handle("/fonts/",http.StripPrefix("/fonts/", http.FileServer(http.Dir("./frontend/fonts"))))
+	http.Handle("/js/",http.StripPrefix("/js/", http.FileServer(http.Dir("./frontend/js"))))
+	http.Handle("/vendor/",http.StripPrefix("/vendor/", http.FileServer(http.Dir("./frontend/vendor"))))
+	http.Handle("/img/",http.StripPrefix("/img/", http.FileServer(http.Dir("./frontend/img"))))
+	http.ListenAndServeTLS(":8083", "cert.pem", "privkey.pem", nil)
 }
